d6680165c2b5a4c4a81fc6e2e9ba1ff49d6a8cba
Jonathan Lorimer <jonathan_lorimer@mac.com>
2025-04-03 09:42:32 -0400

    feat: cleanup submitRequest, finalizeClient, and setupCompletionCallback

--- src/Database/TigerBeetle/Raw/Client.hs
+++ src/Database/TigerBeetle/Raw/Client.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Redundant lambda" #-}
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE LambdaCase #-}
 module Database.TigerBeetle.Raw.Client where
 
@@ -15,11 +14,11 @@ import Foreign.Ptr (Ptr, nullPtr, castPtr)
 import GHC.Natural (Natural)
 import Data.Word
 import Control.Concurrent.STM.TMVar (TMVar, putTMVar, newEmptyTMVar, takeTMVar)
-import Control.Concurrent.STM.TVar (TVar, readTVar, modifyTVar', newTVarIO, writeTVar, stateTVar)
+import Control.Concurrent.STM.TVar (TVar, readTVar, modifyTVar', newTVarIO, writeTVar)
 import Data.IntMap.Strict (IntMap)
 import Data.IntMap.Strict qualified as IM
 import Control.Concurrent.STM.TQueue (TQueue, writeTQueue, newTQueueIO, tryReadTQueue)
-import Control.Concurrent.STM (readTVarIO, atomically)
+import Control.Concurrent.STM (atomically, STM)
 import Data.Maybe (isJust)
 import Control.Monad (when, void, forM_)
 import Data.ByteString (ByteString)
@@ -28,8 +27,9 @@ import Database.TigerBeetle.Raw.Response (TBResponse, decodeResponse, DecodeResp
 import Data.Bifunctor
 import Data.Text.Encoding qualified as TE
 import qualified Data.Vector as V
-import Data.Traversable (for)
 import System.Timeout (timeout)
+import Data.Functor (($>))
+import Foreign.C.Types (CChar)
 
 -- | Whether to start an echo server or a standard server
 data ClientKind = Echo | Standard
@@ -154,22 +154,30 @@ withClient cfg clusterId address action =
 
 finalizeClient :: ClientState -> IO ()
 finalizeClient state = do
+  -- Signal that no new incoming request should proceed
+  atomically $ writeTVar state.csIsShutdown True
+
+  -- N.B. race-condition: Once we release from this transaction
+  -- completionCallbacks may fire and overwrite our shutdown error in the
+  -- request resultVar. Conceivably this is fine, the below block just ensures
+  -- that all submitRequest invocations that are blocked on the request context
+  -- resultVar are allowed to proceed. This should effectively flush all active
+  -- requests since the isShutdown flag should prevent new requests from coming
+  -- in.
+  --
+  -- Process interleaving shouldn't cause any active request entries after
+  -- this block (due to using a stale isShutdown value) because the flag is
+  -- read in the same atomically block as active requests are written to in
+  -- submitRequest function
   atomically $ do
-    -- Signal that no new incoming request should proceed
-    writeTVar state.csIsShutdown True
-    -- Eliminate the active requests feed so that the completion callback
-    -- stops processing responses
-    activeReqs <- stateTVar state.csActiveRequests (, IM.empty)
+    activeReqs <- readTVar state.csActiveRequests 
     -- Iterate through all the request vars and put a client shutdown result 
     -- so that instances of `submitRequest` that are waiting are unblocked
     forM_ (IM.elems activeReqs) $ \context ->
       putTMVar context.resultVar (Left ClientShutdownDuringRequest)
   
-  -- Deinitialize the client
+  -- De-initialize the client
   void $ tbClientDeinit state.csClientPtr
-  
-  
-  -- Fail any pending requests
 
 submitRequest 
   :: ClientState
@@ -180,69 +188,67 @@ submitRequest state operation reqData = do
   -- Scaffold request state
   res <- atomically $ readTVar state.csIsShutdown >>= \case
     True -> pure (Left ClientShutdownDuringRequest)
-    False -> do
+    False -> Right <$> provisionRequestContext state
+
+  case res of
+    Left e -> pure $ Left e 
+    Right context -> withPacketPtrs reqData \packetPtr contextPtr (dataPtr, dataSize) -> do
+      -- TODO: confirm that this is the correct way to assign a context id to a packet
+      poke contextPtr context.contextId
+      let packet = TBPacket
+            { tbPacketUserData = castPtr contextPtr
+            , tbPacketData = castPtr dataPtr
+            , tbPacketDataSize = fromIntegral dataSize
+            , tbPacketUserTag = 0
+            , tbPacketOperation = operation
+            , tbPacketStatus = Ok
+            , tbPacketOpaque = V.empty
+            }
+      poke packetPtr packet
+
+      -- Submit the request
+      submitStatus <- tbClientSubmit state.csClientPtr packetPtr
+
+      case submitStatus of
+        ClientOk -> do
+          -- Wait for the result with a timeout
+          let timeoutMicros = fromIntegral state.csTimeoutMillis * 1000  -- Convert ms to μs
+          result <- timeout timeoutMicros $ atomically $ takeTMVar context.resultVar            
+          case result of
+            Just r -> pure r
+            Nothing -> cleanupRequest state context $> Left (RequestTimeoutError operation reqData)
+
+        errorStatus -> cleanupRequest state context $> Left (ClientError errorStatus)
+  where
+    cleanupRequest :: ClientState -> RequestContext -> IO ()
+    cleanupRequest s ctx = atomically $ do
+      -- Remove from active requests
+      modifyTVar' s.csActiveRequests $ IM.delete (fromIntegral ctx.contextId)
+      -- Recycle the ID
+      writeTQueue s.csFreeRequestIds (fromIntegral ctx.contextId)
+
+    withPacketPtrs :: ByteString -> (Ptr TBPacket -> Ptr Word64 -> (Ptr CChar, Int) -> IO a) -> IO a
+    withPacketPtrs bytes action = 
+      alloca \packetPtr ->
+        alloca (BS.useAsCStringLen bytes . action packetPtr)
+
+    provisionRequestContext :: ClientState -> STM RequestContext
+    provisionRequestContext s = do
       -- Try to reuse an ID from the free list first
-      mFreeId <- tryReadTQueue state.csFreeRequestIds
+      mFreeId <- tryReadTQueue s.csFreeRequestIds
       reqId <- case mFreeId of
         Just freeId -> pure freeId
         Nothing -> do
           -- No free IDs, allocate a new one
           curId <- readTVar state.csNextRequestId
-          writeTVar state.csNextRequestId (curId + 1)
+          writeTVar s.csNextRequestId (curId + 1)
           return curId
 
       -- Create context for this request
       resVar <- newEmptyTMVar
       let context = RequestContext reqId resVar
-  
+
       -- Register the request
-      modifyTVar' state.csActiveRequests $ IM.insert (fromIntegral reqId) context
-      pure $ Right context
+      modifyTVar' s.csActiveRequests $ IM.insert (fromIntegral reqId) context
+      pure context
 
-  case res of
-    Left e -> pure $ Left e 
-    Right context -> do
-      -- Allocate and initialize the packet
-      alloca \packetPtr ->
-        alloca \contextPtr -> do
-          -- TODO: confirm that this is the correct way to assign a context id to a packet
-          poke contextPtr context.contextId
-          -- Fill in the packet
-          BS.useAsCStringLen reqData $ \(dataPtr, dataSize) -> do
-            let packet = TBPacket
-                  { tbPacketUserData = castPtr contextPtr
-                  , tbPacketData = castPtr dataPtr
-                  , tbPacketDataSize = fromIntegral dataSize
-                  , tbPacketUserTag = 0
-                  , tbPacketOperation = operation
-                  , tbPacketStatus = Ok
-                  , tbPacketOpaque = V.empty
-                  }
-            poke packetPtr packet
-      
-            -- Submit the request
-            submitStatus <- tbClientSubmit state.csClientPtr packetPtr
-      
-            case submitStatus of
-              ClientOk -> do
-                -- Wait for the result with a timeout
-                let timeoutMicros = fromIntegral state.csTimeoutMillis * 1000  -- Convert ms to μs
-                result <- timeout timeoutMicros $ atomically $ takeTMVar context.resultVar            
-                case result of
-                  Just r -> pure r
-                  Nothing -> atomically $ do
-                      -- Remove from active requests
-                      modifyTVar' state.csActiveRequests $ IM.delete (fromIntegral context.contextId)
-                      -- Recycle the ID
-                      writeTQueue state.csFreeRequestIds (fromIntegral context.contextId)
-
-                      pure . Left $ RequestTimeoutError operation reqData          
-
-              _ -> do
-                -- Remove the request from the map and return ID to free list
-                atomically $ do
-                  -- Remove from active requests
-                  modifyTVar' state.csActiveRequests $ IM.delete (fromIntegral context.contextId)
-                  -- Recycle the ID
-                  writeTQueue state.csFreeRequestIds (fromIntegral context.contextId)
-                  pure $ Left $ ClientError submitStatus
