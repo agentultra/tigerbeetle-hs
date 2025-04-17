c93cf6382708679f28b087768d93803dd2ef1f1e
J Kenneth King <james@agentultra.com>
2025-04-16 20:18:01 -0400

    Add nix/tigerbeetle.nix (#34)
    Build the `tigerbeetle` binary from the same source we build
    `libtb_client.so`
    
    Fixes: #33

--- flake.nix
+++ flake.nix
@@ -65,6 +65,7 @@
           pkgs.zig
           pkgs.tigerbeetle
           self.packages.${system}.libtb_client
+          self.packages.${system}.tigerbeetle
         ];
       });
 
@@ -76,6 +77,7 @@
     }: {
       tigerbeetle-hs = hsPkgs.tigerbeetle-hs;
       libtb_client = pkgs.callPackage ./nix/libtb_client.nix {src = inputs.tigerbeetle-src;};
+      tigerbeetle = pkgs.callPackage ./nix/tigerbeetle.nix {src = inputs.tigerbeetle-src;};
       default = hsPkgs.tigerbeetle-hs;
       glibc = pkgs.glibc;
     });
