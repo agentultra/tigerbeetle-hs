b04b3841662fe562832ae117cbd63ef1538f708d
James King <james@agentultra.com>
2025-07-30 21:19:27 -0400

    Modify app/Main.hs
    Make it use the async API

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,29 @@
 
 module Main where
 
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
 import Database.TigerBeetle.Account
 import Database.TigerBeetle.Client
-import Database.TigerBeetle.Client.Sync qualified as Sync
+import Database.TigerBeetle.Client.Async qualified as Async
 
 main :: IO ()
 main = do
-  result <- Sync.withClient (ClusterId 0) (Address "3000") $ do
-    -- This should return an error from the server.. neither id nor ledger can be zero
-    Sync.createAccounts [CreateAccount (AccountId 9) (LedgerId 9) (AccountCode 1)]
-  print result
+  result <- newTVarIO Nothing
+
+  let completionCallback _ response = do
+        atomically $ writeTVar result (Just response)
+
+  Async.withClient (ClusterId 0) (Address "3000") (Async.ThreadContext 0) completionCallback $ do
+    Async.createAccounts [CreateAccount (AccountId 9) (LedgerId 9) (AccountCode 1)]
+
+  await result
+
+await :: TVar (Maybe TBResponse) -> IO ()
+await result = do
+  r <- atomically $ readTVar result
+  case r of
+    Nothing -> threadDelay 3000 >> await result
+    Just yay -> print yay
