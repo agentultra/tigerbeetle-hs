9c418ba41167a18f522fad02e9020856729b9be0
James King <james@agentultra.com>
2025-07-30 21:18:01 -0400

    Add async withClient implementation

--- src/Database/TigerBeetle/Client/Async.hs
+++ src/Database/TigerBeetle/Client/Async.hs
@@ -1,27 +1,54 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Database.TigerBeetle.Client.Async where
 
 import Control.Monad.IO.Class
 import Control.Monad.Reader
+import Data.Word
+import Database.TigerBeetle.Account
 import Database.TigerBeetle.Address
 import Database.TigerBeetle.ClusterId
+import Database.TigerBeetle.Internal.FFI.Client
+import Database.TigerBeetle.Raw.Account qualified as Raw
+import Database.TigerBeetle.Raw.Response (TBResponse)
+import Database.TigerBeetle.Raw.Response qualified as Raw
 import Database.TigerBeetle.Raw.Client qualified as Raw
+import Foreign.C.Types
+import Foreign.Storable
 
 data AsyncState = AsyncState { asyncStateClientPtr :: Raw.ClientPtr }
 
-newtype AsyncClientT m a = AsyncClientT { getAsyncClientT :: Reader AsyncState m a }
+newtype AsyncClientT m a = AsyncClientT { getAsyncClient :: ReaderT AsyncState m a }
   deriving (Applicative, Functor, Monad, MonadIO, MonadReader AsyncState)
 
-withClient :: MonadIO m => ClusterId -> Address -> AsyncClientT m TBResponse -> m TBResult
-withClient = undefined
+newtype ThreadContext = ThreadContext { getThreadContext :: Word64 }
 
-createAccounts :: MonadIO m => [CreateAccount] -> AsyncClientT m TBResponse
-createAccounts = undefined
+withClient :: MonadIO m => ClusterId -> Address -> ThreadContext -> (ThreadContext -> TBResponse -> IO ()) -> AsyncClientT m () -> m ()
+withClient clusterId address (ThreadContext userCtxt) callback clientAction = do
+  cb <- liftIO $ Raw.makeCompletionCallback $ \(CUIntPtr ctx) tbPacketPtr _ resultDataPtr resultLen -> do
+    tbPacket <- peek tbPacketPtr
+    tbResponse <- Raw.decodeResponse tbPacket resultDataPtr $ fromIntegral resultLen
+    callback (ThreadContext ctx) tbResponse
+  clientInitResult <- liftIO $ Raw.initClient clusterId address (CUIntPtr userCtxt) cb
+  case clientInitResult of
+    Left err -> error $ show err
+    Right clientPtr -> do
+      let asyncState = AsyncState { asyncStateClientPtr = clientPtr }
+      (`runReaderT` asyncState) . getAsyncClient $ clientAction
 
-lookupAccounts :: MonadIO m => [AccountId] -> AsyncClientT m TBResponse
-lookupAccounts = undefined
+createAccounts :: MonadIO m => [CreateAccount] -> AsyncClientT m ()
+createAccounts createAccountParams = do
+  AsyncState {..} <- ask
+  status <- Raw.submit asyncStateClientPtr Raw.createAccounts createAccountParams
+  case status of
+    ClientOk -> pure ()
+    _ -> error $ show status
 
-getAccountBalances :: MonadIO m => [AccountBalances] -> AsyncClientT m TBResponse
-getAccountBalances = undefined
+-- lookupAccounts :: MonadIO m => [AccountId] -> AsyncClientT m TBResponse
+-- lookupAccounts = undefined
+
+-- getAccountBalances :: MonadIO m => [AccountBalances] -> AsyncClientT m TBResponse
+-- getAccountBalances = undefined
 
 -- getAccountTransfers :: MonadIO m => [AccountTransfers] -> SyncClientT m TB= undefined
 -- getAccountTransfers = undefined
