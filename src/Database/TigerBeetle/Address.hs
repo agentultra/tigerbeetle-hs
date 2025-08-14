897c4fc4678c53951ec241aeaebfde84b4bf660e
James King <james@agentultra.com>
2025-08-14 15:26:43 -0400

    Modify src/Database/TigerBeetle/Address.hs
    Added:
      - haddocks

--- src/Database/TigerBeetle/Address.hs
+++ src/Database/TigerBeetle/Address.hs
@@ -2,5 +2,6 @@ module Database.TigerBeetle.Address where
 
 import Data.Text (Text)
 
+-- | A host address of the Tigerbeetle instance
 newtype Address = Address {getAddress :: Text}
   deriving (Eq, Show)
