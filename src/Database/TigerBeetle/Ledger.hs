6d78967e1e15a5bf21642f34916268244f2b4755
James King <james@agentultra.com>
2025-08-14 15:42:40 -0400

    Modify src/Database/TigerBeetle/Ledger.hs
    Added:
      - haddocks

--- src/Database/TigerBeetle/Ledger.hs
+++ src/Database/TigerBeetle/Ledger.hs
@@ -2,5 +2,6 @@ module Database.TigerBeetle.Ledger where
 
 import Data.Word
 
+-- | Partitions the set of accounts that can transact with each other
 newtype LedgerId = LedgerId { getLedgerId :: Word32 }
   deriving (Eq, Show)
