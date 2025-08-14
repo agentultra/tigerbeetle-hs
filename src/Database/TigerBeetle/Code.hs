38ddba74097779bb5dd8602d152b666dd48e2391
James King <james@agentultra.com>
2025-08-14 15:40:09 -0400

    Modify src/Database/TigerBeetle/Code.hs
    Added:
      - haddocks

--- src/Database/TigerBeetle/Code.hs
+++ src/Database/TigerBeetle/Code.hs
@@ -2,5 +2,11 @@ module Database.TigerBeetle.Code where
 
 import Data.Word
 
+-- | Classifies an account
+--
+-- eg: @100, 101, 102@ are all internal accounts of receivables,
+-- settlements, and holds respectively.
+--
+-- Represented as an unsigned 16-bit integer.
 newtype Code = Code { getCode :: Word16 }
   deriving (Eq, Show)
